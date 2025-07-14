9b2e7ca7ed4d16cd43896e9839ff180f6b282708
Ceastin <ceastinstark@gmail.com>
2025-07-14 12:10:21 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="shipment",
+    versions='0.0.1',
+    author='ceastin',
+    author_email='ceastinstark@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
